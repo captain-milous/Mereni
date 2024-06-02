58044853ea37f00622c73a93e29d6b5fec362037
captain-milous <milda.tesar3@gmail.com>
2024-06-02 14:59:46 +0200

    Mereni

---
+++ CSVHandler.cs
@@ -0,0 +1,57 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mereni
+{
+    public static class CSVHandler
+    {
+        private static readonly object fileLock = new object();
+
+        public static void Save(string devid, Dictionary<string, string> data)
+        {
+            string directoryPath = Path.Combine(Program.Folder, devid);
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            string fileName = DateTime.Now.ToString("yyMMdd") + ".csv";
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            lock (fileLock)
+            {
+                bool fileExists = File.Exists(filePath);
+
+                using (var writer = new StreamWriter(filePath, true))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    if (!fileExists)
+                    {
+                        csv.WriteField("Čas");
+                        csv.WriteField("T0imp1");
+                        csv.WriteField("T1imp1");
+                        csv.WriteField("T0imp2");
+                        csv.WriteField("T1imp2");
+                        csv.WriteField("T0imp3");
+                        csv.WriteField("T1imp3");
+                        csv.WriteField("T0imp4");
+                        csv.WriteField("T1imp4");
+                        csv.NextRecord();
+                    }
+
+                    foreach (var item in data)
+                    {
+                        csv.WriteField(item.Value);
+                    }
+                    csv.NextRecord();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+
+namespace Mereni
+{
+    public class Program
+    {
+        public static string Folder = AppDomain.CurrentDomain.BaseDirectory + "Mereni";
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                if (!Directory.Exists(Folder))
+                {
+                    Directory.CreateDirectory(Folder);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://192.168.1.240:80/");
+
+            try
+            {
+                listener.Start();
+                Console.WriteLine("Listening...");
+
+                while (true)
+                {
+                    try
+                    {
+                        HttpListenerContext context = await listener.GetContextAsync();
+                        HttpListenerRequest request = context.Request;
+                        HttpListenerResponse response = context.Response;
+
+                        if (request.HttpMethod == "GET")
+                        {
+                            var query = request.QueryString;
+                            string devid = query["devid"];
+                            string timestamp = DateTime.Now.ToString("HH:mm:ss");
+                            var data = new Dictionary<string, string>
+                            {
+                                { "Timestamp", timestamp },
+                                { "T0imp1", query["T0imp1"] },
+                                { "T1imp1", query["T1imp1"] },
+                                { "T0imp2", query["T0imp2"] },
+                                { "T1imp2", query["T1imp2"] },
+                                { "T0imp3", query["T0imp3"] },
+                                { "T1imp3", query["T1imp3"] },
+                                { "T0imp4", query["T0imp4"] },
+                                { "T1imp4", query["T1imp4"] }
+                            };
+                            CSVHandler.Save(devid, data);
+
+                            string responseString = "Data received and logged";
+                            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                            response.ContentLength64 = buffer.Length;
+                            Stream output = response.OutputStream;
+                            output.Write(buffer, 0, buffer.Length);
+                            output.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error processing request: {ex.Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Critical error: {ex.Message}");
+            }
+            finally
+            {
+                if (listener.IsListening)
+                {
+                    listener.Stop();
+                }
+                listener.Close();
+            }
+        }
+    }
+}
